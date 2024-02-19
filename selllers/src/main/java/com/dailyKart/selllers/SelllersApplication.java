f67a4af0da217442d2597e0ba0bb498c5b0ccb03
Danish Javed <dev.danish.javed@gmail.com>
2024-02-19 23:46:21 +0530

    Initial commit

---
+++ selllers/src/main/java/com/dailyKart/selllers/SelllersApplication.java
@@ -0,0 +1,13 @@
+package com.dailyKart.selllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelllersApplication.class, args);
+	}
+
+}
---
+++ selllers/src/test/java/com/dailyKart/selllers/SelllersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyKart.selllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
